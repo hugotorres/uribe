0625414ce55d1698a5a0215c02c8cf522ee484d2
Hugo torres <hugotorreslinares@gmail.com>
2020-02-25 23:25:09 -0500

    scrolling with only one character

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <div options="options" id="fullpage" v-if="isLoaded" class="color6">
-      <div v-for="showerThought in showerThoughts" :key="showerThought.id" class="section wrapper">
-        <div class="logo sidebar" @click="update">
-          <img src="./assets/uribe.png" alt="Uribe Velez" height="120px" />
-        </div>
-        <div class="content">
+    <full-page :options="options" id="fullpage" v-if="isLoaded">
+      <div v-for="showerThought in showerThoughts" :key="showerThought.id" class="section">
+        <div class="container">
+          <div class="character-image">
+            <img :src="'./images/'+character.image" alt="shower icon" height="250px" />
+          </div>
           <div class="quote">
             <div class="quote-symbol left">“</div>
-
-            {{ showerThought}}
+            <a :href="character.link">{{ showerThought }}</a>
             <div class="quote-symbol right">“</div>
           </div>
-          <!-- <div class="details">
-              <div class="details-author">Author: {{ showerThought.data.author }}</div>
-              <div class="details-stats">{{ showerThought.data.ups }} Upvote | {{ showerThought.data.num_comments }} Comments</div>
-          </div>-->
+          <div class="details">
+            <div class="details-author">Author: {{ character.name }}</div>
+          </div>
         </div>
       </div>
-
-    
-
-           
-    </div>
+    </full-page>
     <div v-else>
       <loading-spinner />
     </div>
@@ -37,8 +31,9 @@ export default {
     LoadingSpinner
   },
   created() {
-    //fetch("https://www.reddit.com/r/showerthoughts.json?limit=20")
-    fetch("https://old-art-46ef.andrestorres.workers.dev/")
+    fetch(
+      "https://raw.githubusercontent.com/hugotorres/bigbadidea/master/quotes.json"
+    )
       .then(response => response.json())
       .then(data => {
         this.showerThoughts = data;
@@ -48,22 +43,20 @@ export default {
   methods: {
     getColor: function() {
       const numero = Math.floor(Math.random() * 5);
-      console.log(numero);
       const color = this.colors[numero];
-      console.log(color);
       return color;
     },
-    update: function() {
-      this.fetch();
+    update: function(url, name) {
+      this.fetch(url, name);
       const color = this.getColor();
       document.getElementById("fullpage").className = color;
-      console.log(color);
     },
-    fetch: function() {
-      fetch("https://old-art-46ef.andrestorres.workers.dev/")
+    fetch: function(url, alias) {
+      const namex = alias;
+      fetch(url)
         .then(response => response.json())
         .then(data => {
-          this.showerThoughts = data;
+          this.showerThoughts.alias = data.quote;
           this.isLoaded = true;
         });
     }
@@ -71,10 +64,18 @@ export default {
   data() {
     return {
       isLoaded: false,
+      character: {
+        name: "Alvaro Uribe",
+        alias: "uribe",
+        image: "uribe.png",
+        url: "https://old-art-46ef.andrestorres.workers.dev/",
+        link:
+          "https://raw.githubusercontent.com/hugotorres/bigbadidea/master/quotes.json"
+      },
       showerThoughts: [],
       colors: ["color1", "color2", "color3", "color4", "color5", "color6"],
       options: {
-        scrollBar: true,
+        scrollBar: false,
         sectionsColor: [
           "#41b883",
           "#ff5f45",
@@ -95,7 +96,292 @@ export default {
           "#41b883",
           "#ff5f45",
           "#0798ec",
-          "#fec401"
+          "#fec401",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec",
+          "#fec401",
+          "#1bcee6",
+          "#ee1a59",
+          "#ba5be9",
+          "#b4b8ab",
+          "#41b883",
+          "#ff5f45",
+          "#0798ec"
         ]
       }
     };
@@ -109,50 +395,29 @@ export default {
   font-family: sans-serif;
   color: #2d3748;
 }
-#fullpage {
-  height: 100vh;
-}
 body {
   margin: 0;
   padding: 0;
 }
+.character-image {
+  height: 20vh;
+  overflow: hidden;
+}
 h3 {
   margin: 0;
 }
-.color1 {
-  background-color: rgb(41, 162, 136);
+.container {
+  margin: auto;
+  max-width: 800px;
+  padding: 80px 16px;
 }
-.color2 {
-  background-color: #231651;
-}
-.color3 {
-  background: #4dccbd;
-}
-.color4 {
-  background: #2374ab;
-}
-.color5 {
-  background-color: #ff8484;
-}
-.color6 {
-  background-color: rgb(245, 63, 63);
-}
-
 .section {
   text-align: center;
 }
-.logo {
-  cursor: pointer;
-}
-.logo img {
-  height: 80vh;
-  position: absolute;
-  bottom: 0px;
-}
 .quote-symbol {
   font-size: 64px;
-  line-height: 0.5;
-  margin-top: 50px;
+  line-height: 0;
+  margin-top: 20px;
   color: white;
 }
 .quote-symbol.left {
@@ -161,85 +426,17 @@ h3 {
 .quote-symbol.right {
   text-align: right;
 }
-
-.quote {
+.quote a {
   text-decoration: none;
   font-family: "Sriracha", cursive;
-  color: #ffffff;
+  color: white;
   font-size: 32px;
   line-height: 1.5;
-
-  background: #fdfdfd1c;
-  border: radius;
-  border-radius: 31px;
-  -webkit-box-shadow: #5b5b5b52 13px 14px 19px 8px;
-  box-shadow: #5b5b5b52 13px 14px 19px 8px;
 }
-.quote hover {
+.quote a:hover {
   color: #edf2f7;
 }
 .details-stats {
   margin-top: 4px;
-}
-.logo.sidebar:before {
-  content: "";
-  height: 80vh;
-  background: black;
-  bottom: 0;
-  position: absolute;
-  background: url(/assets/uribe.png);
-  background-size: contain;
-  background-repeat: no-repeat;
-  /* transform: skew(19deg, -1deg); */
-  filter: blur(15px);
-}
-.sidebar {
-  grid-area: sidebar;
-}
-
-.content {
-  grid-area: content;
-  position: relative;
-
-  padding: 30px 14%;
-}
-
-.wrapper {
-  display: grid;
-  grid-gap: 0px;
-  grid-template-columns: 33vw 66vw;
-  grid-template-areas: "sidebar content ";
-}
-@media (max-width: 500px) {
-  .logo img {
-    height: 40vh;
-    position: absolute;
-    bottom: 0px;
-  }
-  .wrapper {
-    display: grid;
-    grid-gap: 0px;
-    grid-template-columns: 33vw 66vw;
-    grid-template-areas: "sidebar sidebar" "content content";
-  }
-  .logo.sidebar:before {
-    content: "";
-    height: 250px;
-    background: black;
-    bottom: 0;
-    position: absolute;
-    background: none;
-  }
-  .quote {
-    font-size: 18px;
-    background: #fdfdfd1c;
-    border: radius;
-    border-radius: 31px;
-    -webkit-box-shadow: #5b5b5b52 13px 14px 19px 8px;
-    box-shadow: #5b5b5b52 13px 14px 19px 8px;
-  }
-}
-* {
-  transition: all 0.3s ease-in;
 }
 </style>
